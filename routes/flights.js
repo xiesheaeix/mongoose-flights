d465c0e244880d61e8a9ecc6e458af3fc6cee391
xiesheaeix <sheae123@gmail.com>
2022-10-21 19:13:43 -0700

    add new flight

--- routes/flights.js
+++ routes/flights.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 var flightsCtrl = require('../controllers/flights');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.get('/', flightsCtrl.index);
+router.get('/new', flightsCtrl.new);
+router.post('/', flightsCtrl.create);
 
 module.exports = router;
